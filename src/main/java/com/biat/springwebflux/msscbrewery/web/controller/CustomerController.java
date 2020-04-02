050196c73f72cb4355f09556d95a489d8ce785a7
aymenbraiek <aymen.braiek@esprit.tn>
2020-04-03 00:48:18 +0100

    Spring MVC Controller Advice -closes #3

--- src/main/java/com/biat/springwebflux/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/biat/springwebflux/msscbrewery/web/controller/CustomerController.java
@@ -9,11 +9,16 @@ import org.springframework.core.ReactiveTypeDescriptor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-
+@Validated
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
@@ -27,7 +32,7 @@ public class CustomerController {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity handlePost(CustomerDto customerDto)
+    public ResponseEntity handlePost(@RequestBody @Valid CustomerDto customerDto)
     {
       CustomerDto customerDtosave=customerService.saveCustome(customerDto);
         HttpHeaders httpHeaders=new HttpHeaders();
@@ -35,7 +40,7 @@ public class CustomerController {
         return new ResponseEntity(httpHeaders,HttpStatus.CREATED);
     }
     @PutMapping("/{customerId}")
-    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId , @RequestBody CustomerDto customerDto)
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId ,  @Valid @RequestBody CustomerDto customerDto)
     {
         customerService.updateCustomerById(customerId,customerDto);
         return new ResponseEntity((HttpStatus.NO_CONTENT));
--- src/main/java/com/biat/springwebflux/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/com/biat/springwebflux/msscbrewery/web/model/CustomerDto.java
@@ -1,7 +1,10 @@
 package com.biat.springwebflux.msscbrewery.web.model;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 @Data
 @NoArgsConstructor
@@ -10,5 +13,7 @@ import java.util.UUID;
 @Builder
 public class CustomerDto {
     private UUID id;
+    @NotBlank
+    @Size(min = 3,max = 12)
     private String name;
 }
